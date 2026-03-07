3535a74ddcbf47930a4ca03ea5fc06478b2197aa
tinaz707 <csm347@drexel.edu>
2026-03-06 23:58:39 -0500

    💄Styling Updates + 🐛🔨Navigation FIx
    - fix photography.html layout with media queries to make it go into a grid layout at a desktop view
    - add images/content to artwork.html
    - redo the CSS for the navigation menu, so I start only with the mobile view
    - use .hide + .hidden in my CSS and HTML for menu opening and closing
    - add .addEventListeners to the hamburger icon and close icon function a working open and close navigation menu
    - add sub-menus with property .sub-hide In my HTML + CSS, in JS I create a forEach loop to run to have the drop-downs click and drop down the submenus for both ‘Work’ and ‘About me.’
    - JS: use drop.parentElement.querySelector('.nav-sub-menu') where the drop = variable name for class of ‘.sub-image-text’, then drop.parentElement gets the parent element of that class ,which is <li>. Then .querySelector('.nav-sub-menu')  is getting  ‘nav-sub-menu’ from inside the <li>. This is what allows each drop-down to only toggle to the specific item ‘About me’ or ‘Work’ without eaffectingeach other

--- scripts/main.js
+++ scripts/main.js
@@ -4,20 +4,56 @@ const menu = document.querySelector('.menu-all') //the tabs
 const navbar = document.querySelector('.navbar');
 //click on open and the icon changes to close and the menu pops up
 
-// openMenu.addEventListener("click", () => {
-    // openMenu.classList.toggle("hide");
-    // closeMenu.classList.toggle("hide");
-    // menu.classList.add("active");
-// }, false);
-    
-// closeMenu.addEventListener("click", () => {
-    // closeMenu.classList.toggle("hide");
-    //  openMenu.classList.toggle("hide");
-    //  menu.classList.remove("active");
-// }, false);
 
-openMenu.addEventListener("click", menuMobile, false);
-    function menuMobile(){
-        openMenu.classList.toggle('active');
-        navbar.classList.toggle('active')
-    }
+openMenu.addEventListener("click", () =>{
+    openMenu.classList.toggle('hide');
+    closeMenu.classList.toggle('hide');
+    menu.classList.toggle('hidden');
+})
+
+closeMenu.addEventListener("click", () =>{
+    openMenu.classList.toggle('hide');
+    closeMenu.classList.toggle('hide');
+    menu.classList.toggle('hidden');
+})
+
+//only works for one
+// const dropdownButton = document.querySelector('.sub-image-text');
+// const submenu = document.querySelector('.nav-sub-menu');
+// const dropdown = document.querySelector('.drop-down-img');
+// const undrop = document.querySelector('.un-drop-down-img');
+
+// dropdownButton.addEventListener("click", ()=> {
+//     submenu.classList.toggle('sub-hide');
+
+//     dropdown.classList.toggle('hide');
+//     undrop.classList.toggle('hide');
+// })
+
+
+
+
+const dropdownButton = document.querySelectorAll('.sub-image-text');//get both classes
+
+// two classes -> need a loop
+
+dropdownButton.forEach(drop => { //drop = the class div: sub-image-text
+    const submenu = drop.parentElement.querySelector('.nav-sub-menu'); //parentElement goes up one -> gets the <li> element -> queryselctor then finds the .navsubmenu only in the li element
+    const dropdown = drop.querySelector('.drop-down-img');
+    const undrop = drop.querySelector('.un-drop-down-img');
+
+    drop.addEventListener("click", (event)=> { //event is just a varibale 
+        event.preventDefault(); //lets you click the drop down first not just the link
+        submenu.classList.toggle('sub-hide');
+
+        dropdown.classList.toggle('hide');
+        undrop.classList.toggle('hide');
+    })
+
+} )
+
+
+
+
+
+
