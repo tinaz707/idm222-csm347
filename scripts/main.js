8c9b48a2c2914b0285b7c1b7711b8fff357a9acc
tinaz707 <csm347@drexel.edu>
2026-03-08 00:52:08 -0500

    💄Style Pages + 🔨✨ Working Carousel Feature
    - add updated navigation + src scripts to all pages
    - for JS navigation, remove “event.preventDefault” to all “Work” and “About Me” to still be clickable
    - Update button link for “Check Prototype” in pmtc_cs.html
    - Change font size for ‘Photography’ heading to fit the screen in photography.html
    - Add styles to art_work.html and about_me.html
    - style + script working carousel using a forEach loop so all carousels become functional in art_work.html
    - style + add carousel feature to about_me.html, change the text, and make both the image and text link to its specific page

--- scripts/main.js
+++ scripts/main.js
@@ -43,7 +43,7 @@ dropdownButton.forEach(drop => { //drop = the class div: sub-image-text
     const undrop = drop.querySelector('.un-drop-down-img');
 
     drop.addEventListener("click", (event)=> { //event is just a varibale 
-        event.preventDefault(); //lets you click the drop down first not just the link
+        //lets you click the drop down first not just the link
         submenu.classList.toggle('sub-hide');
 
         dropdown.classList.toggle('hide');
@@ -53,7 +53,44 @@ dropdownButton.forEach(drop => { //drop = the class div: sub-image-text
 } )
 
 
+//carasol
+
+const carousel = document.querySelectorAll('.carousel-item-ul'); //gets both carousels SLIDES part not the whole carousel
 
 
+carousel.forEach(_slide =>{
+    const slides = _slide.querySelectorAll('.carousel-item'); //nodelist for each image from each carousel
+    const carouselDiv = _slide.closest('.carousel'); //.closets looks through all the elements(up the tree) till it finds exactly .carousel
+    const leftButton = carouselDiv.querySelector('.carousel-left-arrow'); //looks in both carousel and gets both the arrows
+    const rightButton = carouselDiv.querySelector('.carousel-right-arrow');
+
+    console.log(slides);
+
+    let start = 0; //carasol starts on the first image of the nodelist from 'slides'
+
+    function updateImage(){
+        _slide.style.transform = `translateX(-${start*100}%)`; //if start is [2] the caraol-items-ul will shift to the left by 200 -> using template literal to calucle and inset in the string 
+    }
+
+    rightButton.addEventListener("click", ()=>{
+        start++; //start +1
+
+        if(start >= slides.length/*0 1 2*/){ //if the image # is greater than or equal to the carasol item, then restart to the first one
+            start=0;
+        }
+        updateImage();
+    });
+
+    leftButton.addEventListener("click", ()=>{
+        start--; //start-1
+
+        if(start < 0){ //if the image# is less then 0 so before the first img then go to the last slide
+            start= slides.length - 1; //thers 3 in the length so 3-1 =2 --> [2] is the last slide 
+        }
+        updateImage();
+    });
+
+
+});
 
 
